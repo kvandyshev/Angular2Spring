6ccc77753a5111683e959aa0622049bd0640164a
User <k.vandyshev@gmail.com>
2022-01-14 17:32:38 +0200

    First test commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CarEntityDetailsComponent } from './car-entity-details/car-entity-details.component';
+import { CarComponent } from './car/car.component'
+import { CreateCarEntityComponent } from './create-car-entity/create-car-entity.component';
+import { UpdateCarEntityComponent } from './update-car-entity/update-car-entity.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'cars', component: CarComponent},
+  {path: 'create-car-entity', component: CreateCarEntityComponent},
+  {path: '', redirectTo: 'cars', pathMatch: 'full'},
+  {path: "update-car-entity/:id", component: UpdateCarEntityComponent}, 
+  {path: "car-entity-details/:id", component: CarEntityDetailsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientModule } from '@angular/common/http' 
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './car/car.component';
+import { CreateCarEntityComponent } from './create-car-entity/create-car-entity.component';
+import { FormsModule } from '@angular/forms';
+import { UpdateCarEntityComponent } from './update-car-entity/update-car-entity.component';
+import { CarEntityDetailsComponent } from './car-entity-details/car-entity-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent,
+    CreateCarEntityComponent,
+    UpdateCarEntityComponent,
+    CarEntityDetailsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    HttpClientModule, 
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
